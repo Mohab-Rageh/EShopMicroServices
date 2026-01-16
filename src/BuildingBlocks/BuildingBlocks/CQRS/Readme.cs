c38df67d887a04cbd071bd9272612baf04afeac9
Mohab Rageh <mohabrageh3@gmail.com>
2026-01-16 20:17:55 +0200

    Introduce CQRS BuildingBlocks and refactor Catalog.API
    Add BuildingBlocks project with CQRS abstractions (ICommand, IQuery, and handlers) using MediatR. Reference BuildingBlocks in Catalog.API and refactor CreateProduct endpoint/handler to use new CQRS interfaces. Integrate Carter for endpoint routing and Marten for persistence. Add .editorconfig for code style, update solution structure, and include documentation for CQRS pattern.

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/Readme.cs
@@ -0,0 +1,5 @@
+﻿/*
+1.Command Handler Type => IRequestHandler<CommandQueryType,ResultType>  //IRequestHandler is from MediatR library
+2.Command Query Type => CommandQueryType : IRequest<ResultType>  //IRequest is from MediatR library and the result is the output from this request
+3.Result Type => ResultType  //This is the type of the result returned by the command handler
+*/
